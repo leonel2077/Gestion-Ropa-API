e81c8f49e7a35d8176f27ff8aa91ea693dee6655
Leonel Ison <leonelison8@gmail.com>
2024-11-02 18:31:24 -0300

    cambio en el controlador de obtener detalles segun id venta

--- src/controllers/saleDetailController.js
+++ src/controllers/saleDetailController.js
@@ -13,15 +13,25 @@ const createSaleDetail = async (req, res) => {
 
 // Obtener todos los detalles de una venta en particular
 const getSaleDetailsBySaleId = async (req, res) => {
-    const { saleId } = req.query;
+    const { saleId } = req.params;
     try {
-        const saleDetails = await db.SaleDetail.findAll({ where: { saleId } });
-        res.status(200).json(saleDetails);
+        const saleDetails = await db.SaleDetail.findAll({ 
+            where: { saleId },
+            include: {
+                model: db.Clothes,
+                attributes: ['name'],
+            },
+        });
+        
+        const totalAmount = saleDetails.reduce((total, detail) => total + (detail.price * detail.quantity), 0);
+
+        res.status(200).json({ totalAmount, SaleDetails: saleDetails });
     } catch (error) {
         console.error(error)
-        res.status(500).json({ error: 'Error obteniendo los detalles de la venta'})
+        res.status(500).json({ error: 'Error obteniendo los detalles de la venta' });
     }
 }
+
 
 // Obtener todos los detalles
 const getAllSaleDetails = async (req, res) => {
